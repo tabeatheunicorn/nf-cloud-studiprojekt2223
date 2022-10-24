b0cb5fb3418331ef932d5c0a29af087ad1f0f7c6
Tabea <tabea@gruppe.ai>
2022-10-24 22:07:30 +0200

    Add logging config

--- nf_cloud_backend/__main__.py
+++ nf_cloud_backend/__main__.py
@@ -1,5 +1,8 @@
+from logging.config import dictConfig
+
 # internal imports
 from nf_cloud_backend.command_line_interface import ComandLineInterface
+
 
 def main():
     """
@@ -8,5 +11,24 @@ def main():
     cli = ComandLineInterface()
     cli.start()
 
+
 if __name__ == "__main__":
+    dictConfig(
+        {
+            "version": 1,
+            "formatters": {
+                "default": {
+                    "format": "[%(filename)s:%(lineno)s - %(funcName)20s()] %(levelname)s in %(module)s:\n\t %(message)s",
+                }
+            },
+            "handlers": {
+                "wsgi": {
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://flask.logging.wsgi_errors_stream",
+                    "formatter": "default",
+                }
+            },
+            "root": {"level": "DEBUG", "handlers": ["wsgi"]},
+        }
+    )
     main()
