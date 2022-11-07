5b16ee7bd3687cc39e315a9625b9828dae03b6ca
Tabea <tabea@gruppe.ai>
2022-11-07 21:53:27 +0100

    Try integrating plotly

---
+++ nf_cloud_frontend/pages/test.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="div">
+</div>
+</template>
+
+<script>
+export default {
+  head(){
+        return {
+            title: "Testpage",
+            script: [
+                {
+                    hid: "plotly",
+                    src: "https://cdn.plot.ly/plotly-2.16.1.min.js",
+                    defer: true,
+                    // callback: () => { this.isPlotlyLoaded = true }
+                }
+            ]
+        }
+    },
+
+    
+    components: {
+    Plotly
+  },
+
+  data() {
+    return {
+      data:[{
+        x: [1,2,3,4],
+        y: [10,15,13,17],
+        type:"scatter"
+      }],
+      layout:{
+        title: "My graph"
+      }
+    }
+  }
+}
+</script>
