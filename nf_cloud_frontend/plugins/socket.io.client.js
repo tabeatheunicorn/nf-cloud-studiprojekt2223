3e5988cc29478eb4a6cb963f8cd614eb05f4be23
Tabea <tabea@gruppe.ai>
2023-01-03 14:26:24 +0100

    Hotfix Bug WS Connection throwing errors
    The commented out code in plugins/socket.io.client.js tries websocket before polling,. This seems to be connected to the websocket error. Should look into this further

--- nf_cloud_frontend/plugins/socket.io.client.js
+++ nf_cloud_frontend/plugins/socket.io.client.js
@@ -1,12 +1,17 @@
 import io from 'socket.io-client'
 
 export default ({ app }, inject) => {
-    inject(
-        'socket',
-        io(
-            app.$config.nf_cloud_backend_ws_url, {
-                transports: ["websocket", "polling"]
-            }
-        )
-    )
+    const socket = io(
+        app.$config.nf_cloud_backend_ws_url,
+        // {
+        //   transports: ['websocket', 'polling'],
+        // }
+      )
+    
+      socket.on('error', (error) => {
+        console.log("Error occured while trying to establish connection to ", app.$config.nf_cloud_backend_ws_url)
+        console.error(error)
+      })
+    
+      inject('socket', socket)
 }
