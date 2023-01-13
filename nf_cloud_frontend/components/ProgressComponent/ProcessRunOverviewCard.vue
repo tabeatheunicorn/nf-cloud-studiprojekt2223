eb9209e6bf8313d30edd51d7c03e6a87d335a131
Tabea <tabea@gruppe.ai>
2023-01-13 08:40:53 +0100

    Add overview for just one run in a card.
    Filtering the messages for the run Name is working as well

--- nf_cloud_frontend/components/ProgressComponent/ProcessRunOverviewCard.vue
+++ nf_cloud_frontend/components/ProgressComponent/ProcessRunOverviewCard.vue
@@ -1,20 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <div class="card-container">
+      <div class="card">
+        <div class="card-header">Run {{ runname }}</div>
+        <div class="card-body">{{ messagesOfRun }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import weblogMessageService from "@/service/weblog-message-service";
+
 export default {
-  name: "Test",
-  created() {},
+  name: "ProcessRunOverviewCard",
+  props: {
+    runname: {
+      type: String,
+      required: true,
+    },
+  },
+
   computed: {
     messages() {
       return weblogMessageService.messages.items;
+    },
+    messagesOfRun() {
+      return weblogMessageService.filterMessagesByRunName(this.messages, this.runname);
     },
   },
   data() {
     return {};
   },
-  props: {},
   methods: {},
 };
 </script>
--- nf_cloud_frontend/pages/progress_test.vue
+++ nf_cloud_frontend/pages/progress_test.vue
@@ -4,6 +4,7 @@
     <websocket-connection-info-modal />
     <div id="progresgraph"></div>
     <status-overview-weblog-messages />
+    <process-run-overview-card :runname="'grave_visvesvaraya'" />
     <weblog-message-list />
   </div>
 </template>
@@ -12,12 +13,14 @@
 import WebsocketConnectionInfoModal from "@/components/ProgressComponent/WebsocketConnectionInfoModal.vue";
 import WeblogMessageList from "@/components/ProgressComponent/WeblogMessageList.vue";
 import StatusOverviewWeblogMessages from "@/components/ProgressComponent/StatusOverviewWeblogMessages";
+import ProcessRunOverviewCard from "@/components/ProgressComponent/ProcessRunOverviewCard";
 
 export default {
   components: {
     WebsocketConnectionInfoModal,
     WeblogMessageList,
     StatusOverviewWeblogMessages,
+    ProcessRunOverviewCard,
   },
   mounted() {
     var plot_data = [
